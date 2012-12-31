8f25c687e8084c037b362febf5d6427238dd5333
Matt Brennan <mattyb1000@gmail.com>
2012-12-31 09:25:32 +0000

    moar polymorphism!

--- livewire.js
+++ livewire.js
@@ -1,123 +1,19 @@
 (function(){
-  var sync, url, PathMatcher, StringMatcher, RegexpMatcher, FunctionMatcher, AlwaysMatcher, Request, Response, Route, method, slice$ = [].slice, toString$ = {}.toString, this$ = this;
+  var sync, url, Request, Response, Router, MatcherRouter, Matcher, StringMatcher, slice$ = [].slice;
   sync = require('sync');
   url = require('url');
-  exports.PathMatcher = PathMatcher = (function(){
-    PathMatcher.displayName = 'PathMatcher';
-    var prototype = PathMatcher.prototype, constructor = PathMatcher;
-    function PathMatcher(){}
-    PathMatcher.extended = bind$(PathMatcher.subclasses || (PathMatcher.subclasses = []), 'push');
-    PathMatcher.create = curry$(function(pathspec){
-      var that;
-      if (that = find(function(it){
-        return it.handles(pathspec);
-      }, this.subclasses)) {
-        return new that(pathspec);
-      } else {
-        throw TypeError("No routers can handle " + pathspec);
-      }
-    });
-    prototype.match = function(){
-      throw TypeError(this.constructor.displayName + " does not implement match");
-    };
-    prototype.extend = function(){
-      throw TypeError(this.constructor.displayName + " does not implement extend");
-    };
-    return PathMatcher;
-  }());
-  StringMatcher = (function(superclass){
-    var prototype = extend$((import$(StringMatcher, superclass).displayName = 'StringMatcher', StringMatcher), superclass).prototype, constructor = StringMatcher;
-    StringMatcher.handles = compose$([
-      (function(it){
-        return it === 'string';
-      }), (function(it){
-        return typeof it;
-      })
-    ]);
-    function StringMatcher(path){
-      var this$ = this;
-      this.params = [];
-      this.reg = partialize$(RegExp, [void 8, 'i'], [0])(
-      function(it){
-        return ("^" + it) + ('/' === last(path) ? '' : '$');
-      }(
-      path.replace(/:([a-z$_][a-z0-9$_]*)/i, function(m, param){
-        this$.params.push(param);
-        return '([^\\/]+)';
-      })));
-    }
-    prototype.match = function(req){
-      return this.reg.test(req.pathname);
-    };
-    prototype.extract = function(req){
-      var ref$;
-      return listToObj(
-      zip(this.params)(
-      tail(
-      (ref$ = this.reg.exec(req.pathname)) != null
-        ? ref$
-        : [])));
-    };
-    return StringMatcher;
-  }(PathMatcher));
-  RegexpMatcher = (function(superclass){
-    var prototype = extend$((import$(RegexpMatcher, superclass).displayName = 'RegexpMatcher', RegexpMatcher), superclass).prototype, constructor = RegexpMatcher;
-    RegexpMatcher.handles = (function(it){
-      return it instanceof RegExp;
-    });
-    function RegexpMatcher(reg){
-      this.reg = reg;
-    }
-    prototype.match = function(req){
-      return this.reg.test(req.pathname);
-    };
-    prototype.extract = function(req){
-      var ref$;
-      return tail((ref$ = this.reg.exec(req.pathname)) != null
-        ? ref$
-        : []);
-    };
-    return RegexpMatcher;
-  }(PathMatcher));
-  FunctionMatcher = (function(superclass){
-    var prototype = extend$((import$(FunctionMatcher, superclass).displayName = 'FunctionMatcher', FunctionMatcher), superclass).prototype, constructor = FunctionMatcher;
-    FunctionMatcher.handles = (function(it){
-      return it instanceof Function;
-    });
-    function FunctionMatcher(func){
-      this.func = func;
-    }
-    prototype.match = function(req){
-      return false !== (this.result = this.func(req.pathname));
-    };
-    prototype.extract = function(req){
-      return this.result;
-    };
-    return FunctionMatcher;
-  }(PathMatcher));
-  AlwaysMatcher = (function(superclass){
-    var prototype = extend$((import$(AlwaysMatcher, superclass).displayName = 'AlwaysMatcher', AlwaysMatcher), superclass).prototype, constructor = AlwaysMatcher;
-    AlwaysMatcher.handles = (function(it){
-      return it === true;
-    });
-    prototype.match = function(){
-      return true;
-    };
-    prototype.extract = function(){
-      return {};
-    };
-    function AlwaysMatcher(){
-      AlwaysMatcher.superclass.apply(this, arguments);
-    }
-    return AlwaysMatcher;
-  }(PathMatcher));
+  String.prototype.pipe = function(it){
+    return it.end(this.constructor(this));
+  };
+  Buffer.prototype.pipe = function(it){
+    return it.end(this);
+  };
   exports.Request = Request = (function(){
     Request.displayName = 'Request';
     var prototype = Request.prototype, constructor = Request;
     prototype.params = {};
     function Request(req){
       var this$ = this instanceof ctor$ ? this : new ctor$;
-      console.log(req);
       import$(this$, req);
       import$(this$, url.parse(req.url, true));
       return this$;
@@ -134,11 +30,37 @@
     } function ctor$(){} ctor$.prototype = prototype;
     return Response;
   }());
-  exports.Route = Route = (function(){
-    Route.displayName = 'Route';
-    var prototype = Route.prototype, constructor = Route;
-    Route.routes = [];
-    Route.error = function(err){
+  exports.Router = Router = (function(){
+    Router.displayName = 'Router';
+    var prototype = Router.prototype, constructor = Router;
+    Router.subclasses = [];
+    Router.extended = function(subclass){
+      return this.subclasses.push(function(){
+        (subclass.instances || (subclass.instances = [])).push(this);
+        return subclass.apply(this, arguments);
+      });
+    };
+    Router.create = function(method){
+      var spec, that;
+      spec = slice$.call(arguments, 1);
+      if (that = find(function(it){
+        return it.supports.apply(it, spec);
+      }, this.subclasses)) {
+        return (function(func, args, ctor) {
+          ctor.prototype = func.prototype;
+          var child = new ctor, result = func.apply(child, args), t;
+          return (t = typeof result)  == "object" || t == "function" ? result || child : child;
+  })(that, [method].concat(slice$.call(spec)), function(){});
+      } else {
+        throw TypeError("No routers can handle " + spec + ".");
+      }
+    };
+    Router.route = function(req){
+      return concatMap(function(it){
+        return it.find(req);
+      }, constructor.subclasses);
+    };
+    Router.error = curry$(function(res, err){
       var ref$;
       if (err != null) {
         res.statusCode = 500;
@@ -147,71 +69,148 @@
           ? ref$
           : err.toString());
       }
+    });
+    Router.find = function(req){
+      return concatMap(function(it){
+        return it.handlers();
+      })(
+      filter(function(it){
+        return it.match(req);
+      }, this.instances));
     };
-    function Route(method, pathspec, func){
-      var i$, len$, fn, this$ = this instanceof ctor$ ? this : new ctor$;
-      this$.method = method;
-      this$.func = func;
-      if (toString$.call(func).slice(8, -1) === 'Array') {
-        for (i$ = 0, len$ = func.length; i$ < len$; ++i$) {
-          fn = func[i$];
-          Route(method, pathspec, fn);
-        }
-      } else {
-        this$.matcher = PathMatcher.create(pathspec);
-        constructor.routes.push(this$);
-      }
+    prototype.match = function(){
+      throw TypeError(this.constructor.displayName + " does not implement match");
+    };
+    prototype.handlers = function(){
+      throw TypeError(this.constructor.displayName + " does not implement handlers");
+    };
+    prototype.extract = function(){
+      throw TypeError(this.constructor.displayName + " does not implement extract");
+    };
+    function Router(){
+      var this$ = this instanceof ctor$ ? this : new ctor$;
+      throw this$.constructor.displayName + " is abstract and can't be instantiated.";
       return this$;
     } function ctor$(){} ctor$.prototype = prototype;
-    return Route;
+    return Router;
   }());
-  for (method in {
-    'ANY': 'ANY',
-    'GET': 'GET',
-    'POST': 'POST',
-    'PUT': 'PUT',
-    'DELETE': 'DELETE',
-    'OPTIONS': 'OPTIONS',
-    'TRACE': 'TRACE',
-    'CONNECT': 'CONNECT',
-    'HEAD': 'HEAD'
-  }) {
-    exports[method] = fn$;
+  function delegate(methods, unto){
+    var i$, len$, method, results$ = {};
+    for (i$ = 0, len$ = methods.length; i$ < len$; ++i$) {
+      method = methods[i$];
+      results$[method] = fn$;
+    }
+    return results$;
+    function fn$(){
+      return this[unto][method].apply(this, arguments);
+    }
   }
-  exports.use = function(it){
-    return Route('ANY', true, it);
-  };
-  exports.use(function(){
-    this.statusCode = 404;
-    return "404 " + this.pathname;
-  });
+  MatcherRouter = (function(){
+    MatcherRouter.displayName = 'MatcherRouter';
+    var prototype = MatcherRouter.prototype, constructor = MatcherRouter;
+    importAll$(prototype, arguments[0]);
+    MatcherRouter.supports = (function(it){
+      return it instanceof Matcher;
+    });
+    prototype.handlers = function(){
+      return [this.handler];
+    };
+    function MatcherRouter(matcher, handler){
+      this.matcher = matcher;
+      this.handler = handler;
+    }
+    return MatcherRouter;
+  }(delegate(['match', 'extract'], extend$('matcher', Router))));
+  Matcher = (function(){
+    Matcher.displayName = 'Matcher';
+    var prototype = Matcher.prototype, constructor = Matcher;
+    function Matcher(){
+      var this$ = this instanceof ctor$ ? this : new ctor$;
+      throw this$.constructor.displayName + " is abstract and can't be instantiated.";
+      return this$;
+    } function ctor$(){} ctor$.prototype = prototype;
+    prototype.match = function(){
+      throw TypeError(this.constructor.displayName + " does not implement match");
+    };
+    prototype.extract = function(){
+      throw TypeError(this.constructor.displayName + " does not implement extract");
+    };
+    return Matcher;
+  }());
+  StringMatcher = (function(superclass){
+    var prototype = extend$((import$(StringMatcher, superclass).displayName = 'StringMatcher', StringMatcher), superclass).prototype, constructor = StringMatcher;
+    function StringMatcher(method, path, handler){
+      var this$ = this instanceof ctor$ ? this : new ctor$;
+      this$.method = method;
+      this$.path = path;
+      this$.handler = handler;
+      this$.params = [];
+      this$.reg = partialize$(RegExp, [void 8, 'i'], [0])(
+      function(it){
+        return ("^" + it) + ('/' === last(path) ? '' : '$');
+      }(
+      path.replace(/:([a-z$_][a-z0-9$_]*)/i, function(m, param){
+        this$.params.push(param);
+        return '([^\\/]+)';
+      })));
+      return this$;
+    } function ctor$(){} ctor$.prototype = prototype;
+    prototype.match = function(req){
+      return this.reg.test(req.pathname);
+    };
+    prototype.extract = function(req){
+      var ref$;
+      return listToObj(
+      zip(this.params)(
+      tail(
+      (ref$ = this.reg.exec(req.pathname)) != null
+        ? ref$
+        : [])));
+    };
+    return StringMatcher;
+  }(Matcher));
   exports.app = function(req, res){
     var this$ = this;
     return sync(function(){
-      var req, res, route;
-      console.log(req.url);
-      req = new Request(req);
-      res = new Response(res);
-      return fold1(curry$(function(x$, y$){
-        return y$(x$);
-      }))((function(){
-        var i$, ref$, len$, results$ = [];
-        for (i$ = 0, len$ = (ref$ = Route.routes).length; i$ < len$; ++i$) {
+      var augReq, tap, fns, res$, i$, ref$, len$, route, e;
+      try {
+        augReq = Request(req);
+        tap = curry$(function(fn, a){
+          fn(a);
+          return a;
+        });
+        res$ = [];
+        for (i$ = 0, len$ = (ref$ = Route.route(req)).length; i$ < len$; ++i$) {
           route = ref$[i$];
-          if (route.match(req)) {
-            import$(req.params, route.extract(req));
-            results$.push(fn$);
-          }
+          res$.push((fn$.call(this$, augReq, res, route)));
         }
-        return results$;
-        function fn$(it){
-          return this.func.sync(req, res, it);
-        }
-      }()));
+        fns = res$;
+        return function(it){
+          return it.on('error', Route.error(res));
+        }(
+        function(it){
+          return it.pipe(augRes);
+        }(
+        fold(curry$(function(x$, y$){
+          return x$(y$);
+        }), "404 " + req.pathname)(
+        fns)));
+      } catch (e$) {
+        e = e$;
+        return Route.error(res, e);
+      }
+      function fn$(augReq, res, route){
+        import$(augReq.params, route.extract(augReq));
+        return function(it){
+          return route.func.sync(augReq, Response(res), it);
+        };
+      }
     });
   };
-  function bind$(obj, key, target){
-    return function(){ return (target || obj)[key].apply(obj, arguments) };
+  function import$(obj, src){
+    var own = {}.hasOwnProperty;
+    for (var key in src) if (own.call(src, key)) obj[key] = src[key];
+    return obj;
   }
   function curry$(f, args){
     return f.length > 1 ? function(){
@@ -220,23 +219,15 @@
         curry$.call(this, f, params) : f.apply(this, params);
     } : f;
   }
+  function importAll$(obj, src){
+    for (var key in src) obj[key] = src[key];
+    return obj;
+  }
   function extend$(sub, sup){
     function fun(){} fun.prototype = (sub.superclass = sup).prototype;
     (sub.prototype = new fun).constructor = sub;
     if (typeof sup.extended == 'function') sup.extended(sub);
     return sub;
-  }
-  function import$(obj, src){
-    var own = {}.hasOwnProperty;
-    for (var key in src) if (own.call(src, key)) obj[key] = src[key];
-    return obj;
-  }
-  function compose$(fs){
-    return function(){
-      var i, args = arguments;
-      for (i = fs.length; i > 0; --i) { args = [fs[i-1].apply(this, args)]; }
-      return args[0];
-    };
   }
   function partialize$(f, args, where){
     return function(){
@@ -246,8 +237,5 @@
       for(i = 0; i < len; ++i) { ta[tw[0]] = params[i]; tw.shift(); }
       return len < wlen && len ? partialize$(f, ta, tw) : f.apply(this, ta);
     };
-  }
-  function fn$(path, funcs){
-    return Route(method, path, funcs);
   }
 }).call(this);
--- livewire.ls
+++ livewire.ls
@@ -3,51 +3,6 @@ require! [sync,url]
 String::pipe = (.end @constructor this)
 Buffer::pipe = (.end this)
 
-class exports.PathMatcher
-	->
-	@extended = @[]subclasses~push
-	@create = (pathspec)-->
-		if find (.handles pathspec), @subclasses
-			new that pathspec
-		else throw TypeError "No routers can handle #pathspec"
-
-	match:  ->throw TypeError "#{@constructor.display-name} does not implement match"
-	extend: ->throw TypeError "#{@constructor.display-name} does not implement extend"
-
-class StringMatcher extends PathMatcher
-	@handles = (typeof)>>(is \string)
-	(path)->
-		@params = []
-		@reg = path.replace /:([a-z$_][a-z0-9$_]*)/i (m,param)~>
-			@params.push param
-			/([^\/]+)/$
-		|> ->"^#{it}"+(if '/' is last path then '' else \$)
-		|> RegExp _,\i
-	match: (req)->@reg.test req.pathname
-	extract: (req)->
-		(@reg.exec req.pathname) ? []
-		|> tail
-		|> zip @params
-		|> list-to-obj
-
-class RegexpMatcher extends PathMatcher
-	@handles = (instanceof RegExp)
-	(@reg)->
-	match: (req)->@reg.test req.pathname
-	extract: (req)->
-		tail (@reg.exec req.pathname) ? []
-
-class FunctionMatcher extends PathMatcher
-	@handles = (instanceof Function)
-	(@func)->
-	match: (req)-> false isnt @result = @func req.pathname
-	extract: (req)->@result
-
-class AlwaysMatcher extends PathMatcher
-	@handles = (is true)
-	match:   -> yes
-	extract: -> {}
-
 class exports.Request
 	params: {}
 	(req)~>
@@ -57,9 +12,25 @@ class exports.Request
 class exports.Response
 	(res)~>import res
 
-class exports.Route
-	@routes = []
 
+class exports.Router
+	@subclasses = []
+	@extended = (subclass)->
+		@subclasses.push ->
+			subclass[]instances.push this # lol wrap constructors i'm so dirty
+			subclass ...
+
+	#create :: Method -> ...Stuff -> Maybe Route
+	@create = (method, ...spec)->
+		if find (.supports ...spec), @subclasses
+			new that method,...spec
+		else throw TypeError "No routers can handle #{spec}."
+
+	#route :: Request -> List Function
+	@route = (req)->
+		concat-map (.find req), @@subclasses
+
+	#error :: Response -> Error -> Nothing
 	@error = (res,err)-->
 		if err?
 			res.status-code = 500
@@ -67,34 +38,77 @@ class exports.Route
 
 			console.error err.stack ? err.to-string!
 
-	(@method,pathspec,@func)~>
-		if typeof! func is \Array
-			for fn in func then Route method,pathspec,fn
-		else
-			@matcher = PathMatcher.create pathspec
-			@@routes.push this
+	@find = (req)-> # default impl, override me plz (or implement .handler)
+		filter (.match req), @instances
+		|> concat-map (.handlers!)
 
-for method of {\ANY \GET \POST \PUT \DELETE \OPTIONS \TRACE \CONNECT \HEAD}
-	exports[method] = (path,funcs)~>Route method,path,funcs
+	match:  ->
+		throw TypeError "#{@constructor.display-name} does not implement match"
+	handlers: ->
+		throw TypeError "#{@constructor.display-name} does not implement handlers"
+	extract: ->
+		throw TypeError "#{@constructor.display-name} does not implement extract"
 
-exports.use = -> Route \ANY true, it
+	~>
+		throw "#{@constructor.display-name} is abstract and can't be instantiated."
 
-exports.use ->
-	@status-code = 404
-	"404 #{@pathname}"
+function delegate methods,unto
+	{[method, ->@[unto][method] ...] for method in methods}
+
+class MatcherRouter implements delegate <[match extract]> \matcher extends Router
+	@supports = (instanceof Matcher)
+	# match:    ->@matcher.match it
+	# extract:  ->@matcher.extract it
+	handlers: ->[@handler]
+	(@matcher,@handler)->
+
+class Matcher
+	~>
+		throw "#{@constructor.display-name} is abstract and can't be instantiated."
+	match:  ->
+		throw TypeError "#{@constructor.display-name} does not implement match"
+	extract: ->
+		throw TypeError "#{@constructor.display-name} does not implement extract"
+
+class StringMatcher extends Matcher
+	(@method,@path,@handler)~>
+		@params = []
+		@reg = path.replace /:([a-z$_][a-z0-9$_]*)/i (m,param)~>
+			@params.push param
+			/([^\/]+)/$
+		|> ->"^#{it}"+(if '/' is last path then '' else \$)
+		|> RegExp _,\i
+
+	#match :: Request -> Boolean
+	match: (req)->@reg.test req.pathname
+
+	#extract :: Request -> Map String Any
+	extract: (req)->
+		(@reg.exec req.pathname) ? []
+		|> tail
+		|> zip @params
+		|> list-to-obj
+
+
+# exports.use = -> Route \ANY true, it
+
+# exports.use ->
+# 	@status-code = 404
+# 	"404 #{@pathname}"
 
 exports.app = (req,res)->
 	sync ~>
 		try
-			aug-req = Request  req
+			aug-req = Request req
 
 			tap = (fn,a)-->fn a; a
 
-			for route in Route.routes when route.matcher.match aug-req
-				aug-req.params import route.matcher.extract aug-req
-				-> route.func.sync aug-req,(Response res),it
-			|> tap console.log
-			|> fold1 (|>)
+			fns = for route in Route.route req then let aug-req,res
+				aug-req.params import route.extract aug-req
+				->route.func.sync aug-req,(Response res),it
+
+			fns
+			|> fold (<|),"404 #{req.pathname}"
 			|> (.pipe aug-res)
 			|> (.on \error Route.error res)
 		catch
